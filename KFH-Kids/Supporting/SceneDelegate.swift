ef096a5da179e58856fe215d4a97ecb97250a8e1
aseel <aseelabdulghafour.129@gmail.com>
2024-05-22 10:06:49 +0300

    welcome page

--- KFH-Kids/Supporting/SceneDelegate.swift
+++ KFH-Kids/Supporting/SceneDelegate.swift
@@ -18,9 +18,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        
+        window.rootViewController = UINavigationController(rootViewController: WelcomeViewController())
         self.window = window
-        self.window?.makeKeyAndVisible()    }
+        self.window?.makeKeyAndVisible()   }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
