f55eb4c7ca6ce05edc2cfb2dd5ea71d914bc81fc
aseel <aseelabdulghafour.129@gmail.com>
2024-05-28 14:40:02 +0300

    complete fixing

--- KFH-Kids/VC/TaskCell.swift
+++ KFH-Kids/VC/TaskCell.swift
@@ -4,6 +4,9 @@
 //    
 //    let descriptionLabel = UILabel()
 //    let pointsLabel = UILabel()
+//    let checkboxButton = UIButton(type: .custom)
+//    
+//    var checkboxAction: (() -> Void)?
 //    
 //    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
 //        super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -18,21 +21,32 @@
 //    func setupSubviews() {
 //        contentView.addSubview(descriptionLabel)
 //        contentView.addSubview(pointsLabel)
+//        contentView.addSubview(checkboxButton)
 //        
 //        descriptionLabel.font = UIFont.systemFont(ofSize: 16)
 //        pointsLabel.font = UIFont.systemFont(ofSize: 14)
 //        pointsLabel.textColor = .gray
+//        
+//        checkboxButton.setImage(UIImage(systemName: "square"), for: .normal)
+//        checkboxButton.setImage(UIImage(systemName: "checkmark.square"), for: .selected)
+//        checkboxButton.addTarget(self, action: #selector(checkboxTapped), for: .touchUpInside)
 //    }
 //    
 //    func setupConstraints() {
-//        descriptionLabel.snp.makeConstraints { make in
+//        checkboxButton.snp.makeConstraints { make in
 //            make.left.equalTo(contentView).offset(10)
+//            make.centerY.equalTo(contentView)
+//            make.width.height.equalTo(24)
+//        }
+//        
+//        descriptionLabel.snp.makeConstraints { make in
+//            make.left.equalTo(checkboxButton.snp.right).offset(10)
 //            make.centerY.equalTo(contentView).offset(-10)
 //            make.right.equalTo(contentView).offset(-10)
 //        }
 //        
 //        pointsLabel.snp.makeConstraints { make in
-//            make.left.equalTo(contentView).offset(10)
+//            make.left.equalTo(checkboxButton.snp.right).offset(10)
 //            make.top.equalTo(descriptionLabel.snp.bottom).offset(5)
 //            make.right.equalTo(contentView).offset(-10)
 //        }
@@ -41,17 +55,24 @@
 //    func configure(with task: Task) {
 //        descriptionLabel.text = task.description
 //        pointsLabel.text = "Points: \(task.points)"
+//        checkboxButton.isSelected = task.isDone
+//    }
+//    
+//    @objc func checkboxTapped() {
+//        checkboxAction?()
 //    }
 //}
 
-
 import UIKit
+import SnapKit
 
 class TaskCell: UITableViewCell {
     
     let descriptionLabel = UILabel()
     let pointsLabel = UILabel()
     let checkboxButton = UIButton(type: .custom)
+    let taskBackgroundView = UIView()
+    let pointsBackgroundView = UIView()
     
     var checkboxAction: (() -> Void)?
     
@@ -66,16 +87,24 @@ class TaskCell: UITableViewCell {
     }
     
     func setupSubviews() {
-        contentView.addSubview(descriptionLabel)
-        contentView.addSubview(pointsLabel)
+        contentView.addSubview(taskBackgroundView)
+        taskBackgroundView.addSubview(descriptionLabel)
+        contentView.addSubview(pointsBackgroundView)
+        pointsBackgroundView.addSubview(pointsLabel)
         contentView.addSubview(checkboxButton)
         
+        taskBackgroundView.layer.cornerRadius = 20
+        pointsBackgroundView.layer.cornerRadius = 20
+        
         descriptionLabel.font = UIFont.systemFont(ofSize: 16)
-        pointsLabel.font = UIFont.systemFont(ofSize: 14)
-        pointsLabel.textColor = .gray
+        descriptionLabel.textColor = .black
+        
+        pointsLabel.font = UIFont.systemFont(ofSize: 16)
+        pointsLabel.textColor = .black
+        pointsLabel.textAlignment = .center
         
         checkboxButton.setImage(UIImage(systemName: "square"), for: .normal)
-        checkboxButton.setImage(UIImage(systemName: "checkmark.square"), for: .selected)
+        checkboxButton.setImage(UIImage(systemName: "checkmark.square.fill"), for: .selected)
         checkboxButton.addTarget(self, action: #selector(checkboxTapped), for: .touchUpInside)
     }
     
@@ -86,23 +115,51 @@ class TaskCell: UITableViewCell {
             make.width.height.equalTo(24)
         }
         
-        descriptionLabel.snp.makeConstraints { make in
+        taskBackgroundView.snp.makeConstraints { make in
             make.left.equalTo(checkboxButton.snp.right).offset(10)
-            make.centerY.equalTo(contentView).offset(-10)
+            make.top.bottom.equalTo(contentView).inset(5)
+            make.right.equalTo(pointsBackgroundView.snp.left).offset(-10)
+            make.height.equalTo(60)
+            make.width.equalTo(100)
+
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.edges.equalTo(taskBackgroundView).inset(10)
+        }
+        
+        pointsBackgroundView.snp.makeConstraints { make in
+            make.centerY.equalTo(contentView)
             make.right.equalTo(contentView).offset(-10)
+            make.width.height.equalTo(60)
         }
         
         pointsLabel.snp.makeConstraints { make in
-            make.left.equalTo(checkboxButton.snp.right).offset(10)
-            make.top.equalTo(descriptionLabel.snp.bottom).offset(5)
-            make.right.equalTo(contentView).offset(-10)
+            make.edges.equalTo(pointsBackgroundView).inset(5)
         }
     }
     
     func configure(with task: Task) {
         descriptionLabel.text = task.description
-        pointsLabel.text = "Points: \(task.points)"
+        pointsLabel.text = "\(task.points)"
         checkboxButton.isSelected = task.isDone
+        
+        switch task.taskType {
+        case "Chore":
+            taskBackgroundView.backgroundColor = UIColor(red: 0.29, green: 0.89, blue: 0.76, alpha: 1.0)
+            pointsBackgroundView.backgroundColor = UIColor(red: 0.29, green: 0.89, blue: 0.76, alpha: 1.0)
+        case "Homework":
+            taskBackgroundView.backgroundColor = UIColor(red: 1.0, green: 0.82, blue: 0.546, alpha: 1.0)
+            pointsBackgroundView.backgroundColor = UIColor(red: 1.0, green: 0.82, blue: 0.546, alpha: 1.0)
+            
+        case "Done":
+            taskBackgroundView.backgroundColor = #colorLiteral(red: 1, green: 0.5535743833, blue: 0.7493770123, alpha: 1)
+        
+            pointsBackgroundView.backgroundColor = #colorLiteral(red: 1, green: 0.5535743833, blue: 0.7493770123, alpha: 1)
+        default:
+            taskBackgroundView.backgroundColor = UIColor.black
+            pointsBackgroundView.backgroundColor = UIColor(red: 1.0, green: 0.56, blue: 0.56, alpha: 1.0)
+        }
     }
     
     @objc func checkboxTapped() {
--- KFH-Kids/VC/TasksViewController.swift
+++ KFH-Kids/VC/TasksViewController.swift
@@ -1,10 +1,6 @@
-////
-////  TaskViewController.swift
-////  KFH-Kids
-////
-////  Created by Razan alshatti on 22/05/2024.
-////
+
 //import UIKit
+//import SnapKit
 //
 //class TasksViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 //
@@ -36,13 +32,12 @@
 //        // Load your tasks data here
 //        // This is just a sample. Replace it with your actual data loading logic.
 //        tasks = [
-//            Task(description: "Clean your room", points: 10, dueDate: Date(), isDone: false),
-//            Task(description: "Do homework", points: 20, dueDate: Date().addingTimeInterval(-86400), isDone: false),
-//            Task(description: "Wash dishes", points: 5, dueDate: Date().addingTimeInterval(-172800), isDone: true)
+//            Task(id: 1, ParentId: 101, taskType: "Chore", description: "Clean your room", date: Date(), points: 10, children: "Child1", dueDate: Date(), isDone: false),
+//            Task(id: 2, ParentId: 102, taskType: "Homework", description: "Do homework", date: Date().addingTimeInterval(-86400), points: 20, children: "Child2", dueDate: Date().addingTimeInterval(-86400), isDone: false),
+//            Task(id: 3, ParentId: 103, taskType: "Chore", description: "Wash dishes", date: Date().addingTimeInterval(-172800), points: 5, children: "Child3", dueDate: Date().addingTimeInterval(-172800), isDone: true)
 //        ]
 //        tableView.reloadData()
 //    }
-//
 //    
 //    func numberOfSections(in tableView: UITableView) -> Int {
 //        return 3
@@ -57,6 +52,22 @@
 //        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as! TaskCell
 //        let task = tasksForSection(indexPath.section)[indexPath.row]
 //        cell.configure(with: task)
+//        
+//        cell.checkboxAction = { [weak self] in
+//            guard let self = self else { return }
+//            var task = self.tasksForSection(indexPath.section)[indexPath.row]
+//            task.isDone.toggle()
+//            if task.isDone {
+//                self.totalPoints += task.points
+//            } else {
+//                self.totalPoints -= task.points
+//            }
+//            if let index = self.tasks.firstIndex(where: { $0.id == task.id }) {
+//                self.tasks[index].isDone = task.isDone
+//            }
+//            self.tableView.reloadData()
+//        }
+//        
 //        return cell
 //    }
 //    
@@ -66,19 +77,6 @@
 //        case 1: return "Past"
 //        case 2: return "Done"
 //        default: return nil
-//        }
-//    }
-//    
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        var task = tasksForSection(indexPath.section)[indexPath.row]
-//        if !task.isDone && indexPath.section != 2 {
-//            task.isDone = true
-//            totalPoints += task.points
-//            // Update the task in the original tasks array
-//            if let index = tasks.firstIndex(where: { $0.description == task.description }) {
-//                tasks[index].isDone = true
-//            }
-//            tableView.reloadData()
 //        }
 //    }
 //    
@@ -110,7 +108,7 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        title = "Assigned Tasks"
+        title = "Tasks"
         
         setupTableView()
         loadTasks()
@@ -133,7 +131,7 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
         tasks = [
             Task(id: 1, ParentId: 101, taskType: "Chore", description: "Clean your room", date: Date(), points: 10, children: "Child1", dueDate: Date(), isDone: false),
             Task(id: 2, ParentId: 102, taskType: "Homework", description: "Do homework", date: Date().addingTimeInterval(-86400), points: 20, children: "Child2", dueDate: Date().addingTimeInterval(-86400), isDone: false),
-            Task(id: 3, ParentId: 103, taskType: "Chore", description: "Wash dishes", date: Date().addingTimeInterval(-172800), points: 5, children: "Child3", dueDate: Date().addingTimeInterval(-172800), isDone: true)
+            Task(id: 3, ParentId: 103, taskType: "Done", description: "Watch TV for 1 hour", date: Date().addingTimeInterval(-172800), points: 5, children: "Child3", dueDate: Date().addingTimeInterval(-172800), isDone: true)
         ]
         tableView.reloadData()
     }
