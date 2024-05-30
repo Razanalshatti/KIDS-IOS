5f588f8f919c8810be196e6870fe184a96b52e16
razanalshatti <83327@kfh.com>
2024-05-30 15:47:37 +0300

    pass user

--- KFH-Kids/VC/HomeViewController.swift
+++ KFH-Kids/VC/HomeViewController.swift
@@ -1,341 +1,3 @@
-//
-//import UIKit
-//import SnapKit
-//import Eureka
-//import Alamofire
-//import Kingfisher
-//
-//class HomeViewController: UIViewController {
-//
-//    let screenImage = UIImageView()
-//    let usernameLabel = UILabel()
-//    let coinIcon = UIImageView()
-//    let cardIcon = UIButton(type: .custom)
-//    let profileIcon = UIImageView()
-//    let rewardButton1 = UIButton(type: .custom)
-//    let rewardButton2 = UIButton(type: .custom)
-//    let rewardButton3 = UIButton(type: .custom)
-//    let transferPtsToGold = UIButton(type: .system)
-//    let transferPtsToMoney = UIButton(type: .system)
-//    let bonus = UIButton(type: .system)
-//    let rewardsLabel = UILabel()
-//    let rewardsSubtitle = UILabel()
-//    let serviceLabel = UILabel()
-//    let coinLabel = UILabel()
-//    var bonusButtonTimer: Timer?
-//    var bonusButtonDisabledUntil: Date?
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        view.backgroundColor = .white
-//        setupSubviews()
-//        setupConstraints()
-//        checkBonusButtonState()
-//    }
-//
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        navigationController?.isNavigationBarHidden = true
-//    }
-//
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//        navigationController?.isNavigationBarHidden = false
-//    }
-//
-//    func setupSubviews() {
-//        // Profile Icon
-//        profileIcon.image = UIImage(named: "profile")
-//        profileIcon.contentMode = .scaleAspectFill
-//        profileIcon.clipsToBounds = true
-//        profileIcon.layer.cornerRadius = 20
-//        view.addSubview(profileIcon)
-//
-//        // Username Label
-//        usernameLabel.text = "Hi, Fahad"
-//        usernameLabel.font = UIFont.boldSystemFont(ofSize: 18)
-//        usernameLabel.textColor = .black
-//        view.addSubview(usernameLabel)
-//
-//        // Coin Icon and Label
-//        coinIcon.image = UIImage(named: "favourites")
-//        coinIcon.contentMode = .scaleAspectFit
-//        view.addSubview(coinIcon)
-//
-//        coinLabel.text = "10"
-//        coinLabel.font = UIFont.systemFont(ofSize: 18)
-//        view.addSubview(coinLabel)
-//
-//        // Card Icon (Saving)
-//        cardIcon.setImage(UIImage(named: "saving"), for: .normal)
-//        cardIcon.contentMode = .scaleAspectFit
-//        cardIcon.clipsToBounds = true
-//        cardIcon.addTarget(self, action: #selector(navigateToBankCard), for: .touchUpInside)
-//        view.addSubview(cardIcon)
-//
-//        // Background Image
-//        screenImage.image = UIImage(named: "background")
-//        screenImage.contentMode = .scaleAspectFill
-//        view.addSubview(screenImage)
-//
-//        // Rewards Label
-//        rewardsLabel.text = "Rewards"
-//        rewardsLabel.font = UIFont.boldSystemFont(ofSize: 20)
-//        rewardsLabel.textColor = .black
-//        view.addSubview(rewardsLabel)
-//
-//        // Rewards Subtitle
-//        rewardsSubtitle.text = "Complete tasks to get points"
-//        rewardsSubtitle.font = UIFont.systemFont(ofSize: 12)
-//        rewardsSubtitle.textColor = .lightGray
-//        view.addSubview(rewardsSubtitle)
-//
-//        // Reward Buttons
-//        rewardButton1.setBackgroundImage(UIImage(named: "reward"), for: .normal)
-//        rewardButton1.contentMode = .scaleAspectFill
-//        view.addSubview(rewardButton1)
-//
-//        rewardButton2.setBackgroundImage(UIImage(named: "reward"), for: .normal)
-//        rewardButton2.contentMode = .scaleAspectFill
-//        view.addSubview(rewardButton2)
-//        
-//        rewardButton3.setBackgroundImage(UIImage(named: "reward"), for: .normal)
-//        rewardButton3.contentMode = .scaleAspectFill
-//        view.addSubview(rewardButton3)
-//
-//        // Service Section Label
-//        serviceLabel.text = "Services"
-//        serviceLabel.font = UIFont.boldSystemFont(ofSize: 20)
-//        serviceLabel.textColor = .black
-//        view.addSubview(serviceLabel)
-//
-//        // Transfer Points to Gold Button
-//        transferPtsToGold.setTitle("Transfer Points To Gold", for: .normal)
-//        styleButton(transferPtsToGold, color: UIColor(red: 1.0, green: 0.796, blue: 0.486, alpha: 1.0))
-//        transferPtsToGold.layer.cornerRadius = 20
-//        transferPtsToGold.layer.masksToBounds = true
-//        transferPtsToGold.addTarget(self, action: #selector(transferToGoldButtonTapped), for: .touchUpInside)
-//        view.addSubview(transferPtsToGold)
-//
-//        // Transfer Points to Money Button
-//        transferPtsToMoney.setTitle("Transfer Points To Money", for: .normal)
-//        styleButton(transferPtsToMoney, color: UIColor(red: 1.0, green: 0.796, blue: 0.486, alpha: 1.0))
-//        transferPtsToMoney.layer.cornerRadius = 20
-//        transferPtsToMoney.layer.masksToBounds = true
-//        view.addSubview(transferPtsToMoney)
-//        
-//        // Bonus Button
-//        bonus.setTitle("Extra Points!", for: .normal)
-//        styleButton(bonus, color: UIColor(red: 0.451, green: 0.859, blue: 0.835, alpha: 1.0))
-//        bonus.layer.cornerRadius = 18
-//        bonus.layer.masksToBounds = true
-//        bonus.addTarget(self, action: #selector(bonusButtonTapped), for: .touchUpInside)
-//        view.addSubview(bonus)
-//    }
-//
-//    func styleButton(_ button: UIButton, color: UIColor) {
-//        button.backgroundColor = color
-//        button.setTitleColor(.black, for: .normal)
-//        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-//        button.layer.cornerRadius = 10
-//    }
-//
-//    @objc func bonusButtonTapped() {
-//        let vc = ActivityViewController()
-//        vc.modalPresentationStyle = .pageSheet
-//        vc.delegate = self
-//
-//        if let sheet = vc.sheetPresentationController {
-//            sheet.detents = [.medium(), .large()]
-//            sheet.prefersGrabberVisible = true
-//        }
-//
-//        present(vc, animated: true, completion: nil)
-//    }
-//
-//    @objc func transferToGoldButtonTapped() {
-//        let vc = TransferPointsToGoldViewController()
-//        vc.modalPresentationStyle = .pageSheet
-//        vc.delegate = self
-//
-//        if let sheet = vc.sheetPresentationController {
-//            sheet.detents = [.medium(), .large()]
-//            sheet.prefersGrabberVisible = true
-//        }
-//
-//        present(vc, animated: true, completion: nil)
-//    }
-//
-//    @objc func navigateToBankCard() {
-//        let bankCardVC = BankCardViewController()
-//        navigationController?.pushViewController(bankCardVC, animated: true)
-//    }
-//
-//    func disableBonusButton() {
-//        bonus.isEnabled = false
-//        bonus.backgroundColor = .gray
-//
-//        bonusButtonDisabledUntil = Date().addingTimeInterval(1)
-//        UserDefaults.standard.set(bonusButtonDisabledUntil, forKey: "bonusButtonDisabledUntil")
-//
-//        startBonusButtonTimer()
-//    }
-//
-//    func checkBonusButtonState() {
-//        if let disabledUntil = UserDefaults.standard.object(forKey: "bonusButtonDisabledUntil") as? Date {
-//            bonusButtonDisabledUntil = disabledUntil
-//
-//            if Date() < disabledUntil {
-//                disableBonusButton()
-//                startBonusButtonTimer()
-//            } else {
-//                enableBonusButton()
-//            }
-//        }
-//    }
-//
-//    func enableBonusButton() {
-//        bonus.isEnabled = true
-//        bonus.backgroundColor = UIColor(red: 0.451, green: 0.859, blue: 0.835, alpha: 1.0)
-//        bonusButtonDisabledUntil = nil
-//        UserDefaults.standard.removeObject(forKey: "bonusButtonDisabledUntil")
-//    }
-//
-//    func startBonusButtonTimer() {
-//        bonusButtonTimer?.invalidate()
-//        bonusButtonTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-//            if let disabledUntil = self.bonusButtonDisabledUntil {
-//                let remainingTime = Int(disabledUntil.timeIntervalSinceNow)
-//
-//                if remainingTime <= 0 {
-//                    self.enableBonusButton()
-//                    timer.invalidate()
-//                } else {
-//                    let hours = remainingTime / 3600
-//                    let minutes = (remainingTime % 3600) / 60
-//                    let seconds = remainingTime % 60
-//                    self.bonus.setTitle(String(format: "Bonus (%02d:%02d:%02d)", hours, minutes, seconds), for: .disabled)
-//                }
-//            }
-//        }
-//    }
-//
-//    func setupConstraints() {
-//        
-//        //MARK: Header:
-//        profileIcon.snp.makeConstraints { make in
-//            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-//            make.left.equalTo(view).offset(20)
-//            make.width.height.equalTo(40)
-//        }
-//        usernameLabel.snp.makeConstraints { make in
-//            make.centerY.equalTo(profileIcon)
-//            make.left.equalTo(profileIcon.snp.right).offset(10)
-//        }
-//        coinIcon.snp.makeConstraints { make in
-//            make.centerY.equalTo(profileIcon)
-//            make.right.equalTo(cardIcon.snp.left).offset(-10)
-//            make.width.height.equalTo(30)
-//        }
-//        coinLabel.snp.makeConstraints { make in
-//            make.centerY.equalTo(profileIcon)
-//            make.right.equalTo(coinIcon.snp.left).offset(-5)
-//        }
-//        cardIcon.snp.makeConstraints { make in
-//            make.centerY.equalTo(profileIcon)
-//            make.right.equalTo(view).offset(-20)
-//            make.width.equalTo(40) // Adjusted size
-//            make.height.equalTo(30) // Adjusted size
-//        }
-//
-//
-//        
-//        //MARK: background image:
-//        
-//        screenImage.snp.makeConstraints { make in
-//            make.top.equalTo(profileIcon.snp.bottom).offset(20)
-//            make.left.right.equalTo(view)
-//            make.height.equalTo(view).multipliedBy(0.3)
-//        }
-//        
-//        // MARK: rewards and service section
-//
-//      
-//        rewardsLabel.snp.makeConstraints { make in
-//            make.top.equalTo(screenImage.snp.bottom).offset(20)
-//            make.left.equalTo(view).offset(20)
-//        }
-//
-//        rewardsSubtitle.snp.makeConstraints { make in
-//            make.top.equalTo(rewardsLabel.snp.bottom).offset(5)
-//            make.left.equalTo(view).offset(20)
-//        }
-//
-//        rewardButton1.snp.makeConstraints { make in
-//            make.top.equalTo(rewardsSubtitle.snp.bottom).offset(10)
-//            make.left.equalTo(view).offset(20)
-//            make.width.equalTo(100)
-//            make.height.equalTo(70)
-//        }
-//
-//        rewardButton2.snp.makeConstraints { make in
-//            make.top.equalTo(rewardsSubtitle.snp.bottom).offset(10)
-//            make.left.equalTo(rewardButton1.snp.right).offset(20)
-//            make.width.equalTo(100)
-//            make.height.equalTo(70)
-//        }
-//        
-//        rewardButton3.snp.makeConstraints { make in
-//            make.top.equalTo(rewardsSubtitle.snp.bottom).offset(10)
-//            make.left.equalTo(rewardButton2.snp.right).offset(20)
-//            make.width.equalTo(100)
-//            make.height.equalTo(70)
-//        }
-//
-//        serviceLabel.snp.makeConstraints { make in
-//            make.top.equalTo(rewardButton1.snp.bottom).offset(20)
-//            make.left.equalTo(view).offset(20)
-//        }
-//
-//        transferPtsToGold.snp.makeConstraints { make in
-//            make.top.equalTo(serviceLabel.snp.bottom).offset(10)
-//            make.left.equalTo(view).offset(30)
-//            make.width.equalTo(327)
-//            make.height.equalTo(73)
-//        }
-//
-//        transferPtsToMoney.snp.makeConstraints { make in
-//            make.top.equalTo(transferPtsToGold.snp.bottom).offset(10)
-//            make.left.equalTo(view).offset(30)
-//            make.width.equalTo(327)
-//            make.height.equalTo(73)
-//        }
-//
-//        bonus.snp.makeConstraints { make in
-//            make.top.equalTo(transferPtsToMoney.snp.bottom).offset(10)
-//            make.left.equalTo(view).offset(30)
-//            make.width.equalTo(322)
-//            make.height.equalTo(69)
-//        }
-//    }
-//}
-//
-//// Ensure that HomeViewController conforms to ActivityViewControllerDelegate
-//extension HomeViewController: ActivityViewControllerDelegate {
-//    func didCompleteActivity() {
-//        disableBonusButton()
-//    }
-//}
-//
-//// Ensure that HomeViewController conforms to TransferPointsToGoldDelegate
-//extension HomeViewController: TransferPointsToGoldDelegate {
-//    func removeBlurEffect() {
-//        // No blur effect to remove
-//    }
-//}
-//    
-
-
 
 import UIKit
 import SnapKit
@@ -363,17 +25,12 @@ class HomeViewController: UIViewController {
     var bonusButtonTimer: Timer?
     var bonusButtonDisabledUntil: Date?
 
-    var child: Child?
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupSubviews()
         setupConstraints()
         checkBonusButtonState()
-        if let child = child {
-            updateUI(with: child)
-        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -395,7 +52,7 @@ class HomeViewController: UIViewController {
         view.addSubview(profileIcon)
 
         // Username Label
-        usernameLabel.text = "Hi, \(child?.Username)"
+        usernameLabel.text = "Hi, Fahad"
         usernameLabel.font = UIFont.boldSystemFont(ofSize: 18)
         usernameLabel.textColor = .black
         view.addSubview(usernameLabel)
@@ -405,6 +62,7 @@ class HomeViewController: UIViewController {
         coinIcon.contentMode = .scaleAspectFit
         view.addSubview(coinIcon)
 
+        coinLabel.text = "10"
         coinLabel.font = UIFont.systemFont(ofSize: 18)
         view.addSubview(coinLabel)
 
@@ -563,6 +221,7 @@ class HomeViewController: UIViewController {
     }
 
     func setupConstraints() {
+        
         //MARK: Header:
         profileIcon.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
@@ -589,14 +248,19 @@ class HomeViewController: UIViewController {
             make.height.equalTo(30) // Adjusted size
         }
 
+
+        
         //MARK: background image:
+        
         screenImage.snp.makeConstraints { make in
             make.top.equalTo(profileIcon.snp.bottom).offset(20)
             make.left.right.equalTo(view)
             make.height.equalTo(view).multipliedBy(0.3)
         }
-
+        
         // MARK: rewards and service section
+
+      
         rewardsLabel.snp.makeConstraints { make in
             make.top.equalTo(screenImage.snp.bottom).offset(20)
             make.left.equalTo(view).offset(20)
@@ -654,11 +318,6 @@ class HomeViewController: UIViewController {
             make.height.equalTo(69)
         }
     }
-
-    func updateUI(with child: Child) {
-        usernameLabel.text = "Hi, \(child.Username)"
-        coinLabel.text = "\(child.Points)"
-    }
 }
 
 // Ensure that HomeViewController conforms to ActivityViewControllerDelegate
@@ -674,3 +333,5 @@ extension HomeViewController: TransferPointsToGoldDelegate {
         // No blur effect to remove
     }
 }
+    
+
--- KFH-Kids/VC/LoginViewController.swift
+++ KFH-Kids/VC/LoginViewController.swift
@@ -1,3 +1,158 @@
+//
+//
+//import UIKit
+//import SnapKit
+//
+//class LoginViewController: UIViewController {
+//    var background = UIImageView()
+//    var usernameTextField = UITextField()
+//    var passwordTextField = UITextField()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        setupBackground()
+//        setupLoginLabel()
+//        setupUsernameTextField()
+//        setupPasswordTextField()
+//        setupLoginButton()
+//    }
+//
+//    func setupBackground() {
+//        background = UIImageView()
+//        background.image = UIImage(named: "welcome")
+//        background.contentMode = .scaleAspectFill
+//        background.clipsToBounds = true
+//
+//        view.addSubview(background)
+//
+//        background.snp.makeConstraints { make in
+//            make.edges.equalToSuperview()
+//        }
+//    }
+//
+//    private func setupLoginLabel() {
+//        let loginLabel = UILabel()
+//        loginLabel.text = "Login"
+//        loginLabel.textColor = UIColor(red: 1.0, green: 0.796, blue: 0.486, alpha: 1.0)
+//        loginLabel.font = UIFont.boldSystemFont(ofSize: 39)
+//        
+//        self.view.addSubview(loginLabel)
+//        
+//        loginLabel.snp.makeConstraints { make in
+//            make.leading.equalTo(self.view).offset(150)
+//            make.top.equalTo(self.view).offset(190)
+//        }
+//    }
+//
+//    private func setupUsernameTextField() {
+//        usernameTextField.placeholder = "Username"
+//        usernameTextField.backgroundColor = UIColor(white: 0.9, alpha: 1)
+//        usernameTextField.layer.cornerRadius = 20
+//        usernameTextField.setLeftPaddingPoints(10)
+//        
+//        self.view.addSubview(usernameTextField)
+//        
+//        usernameTextField.snp.makeConstraints { make in
+//            make.centerX.equalTo(self.view)
+//            make.top.equalTo(self.view).offset(308)
+//            make.width.equalTo(295)
+//            make.height.equalTo(40)
+//        }
+//    }
+//
+//    private func setupPasswordTextField() {
+//        passwordTextField.placeholder = "Password"
+//        passwordTextField.backgroundColor = UIColor(white: 0.9, alpha: 1)
+//        passwordTextField.layer.cornerRadius = 20
+//        passwordTextField.isSecureTextEntry = true
+//        passwordTextField.setLeftPaddingPoints(10)
+//        
+//        self.view.addSubview(passwordTextField)
+//        
+//        passwordTextField.snp.makeConstraints { make in
+//            make.centerX.equalTo(self.view)
+//            make.top.equalTo(usernameTextField.snp.bottom).offset(50)
+//            make.width.equalTo(295)
+//            make.height.equalTo(40)
+//        }
+//    }
+//
+//    private func setupLoginButton() {
+//        let loginButton = UIButton(type: .system)
+//        loginButton.setTitle("Let's Go", for: .normal)
+//        loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+//        loginButton.backgroundColor = UIColor(red: 1, green: 0.797, blue: 0.488, alpha: 1)
+//        loginButton.layer.cornerRadius = 20
+//        loginButton.setTitleColor(.white, for: .normal)
+//
+//        loginButton.layer.shadowColor = UIColor(red: 0.933, green: 0.933, blue: 0.933, alpha: 1).cgColor
+//        loginButton.layer.shadowOpacity = 1
+//        loginButton.layer.shadowRadius = 4
+//        loginButton.layer.shadowOffset = CGSize(width: 0, height: 4)
+//        loginButton.layer.shadowPath = UIBezierPath(roundedRect: loginButton.bounds, cornerRadius: 20).cgPath
+//        
+//        self.view.addSubview(loginButton)
+//        
+//        loginButton.snp.makeConstraints { make in
+//            make.width.equalTo(153)
+//            make.height.equalTo(40)
+//            make.centerX.equalTo(self.view)
+//            make.top.equalTo(self.view).offset(532)
+//        }
+//        
+//        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+//    }
+//
+//    @objc func loginButtonTapped() {
+//        let username = usernameTextField.text
+//        let password = passwordTextField.text
+//        
+//        if username?.isEmpty ?? true || password?.isEmpty ?? true {
+//            let message = """
+//            Please enter the required:
+//            \(username?.isEmpty ?? true ? "Username" : "")
+//            \(password?.isEmpty ?? true ? "Password" : "")
+//            """
+//            presentAlertWithTitle(title: "Error", message: message)
+//            return
+//        }
+//        
+//        let child = Child(Id: 1, Username: username!, Password: password!, ParentId: 1, Points: 5, BaitiAccountNumber: 54, SavingsAccountNumber: 6789, Balance: 129, Tasks: "", request: username!)
+//        
+//        NetworkManager.shared.login(child: child) { [weak self] result in
+//            switch result {
+//            case .success:
+//                DispatchQueue.main.async {
+//                    let homeViewController = HomeViewController()
+//                    self?.navigationController?.pushViewController(homeViewController, animated: true)
+//                }
+//            case .failure:
+//                DispatchQueue.main.async {
+//                    self?.presentAlertWithTitle(title: "Error", message: "Username or password is incorrect")
+//                }
+//            }
+//        }
+//    }
+//    
+//    func presentAlertWithTitle(title: String, message: String, completion: (() -> Void)? = nil) {
+//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//        self.present(alert, animated: true, completion: nil)
+//        
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//            alert.dismiss(animated: true, completion: completion)
+//        }
+//    }
+//}
+//
+//extension UITextField {
+//    func setLeftPaddingPoints(_ amount: CGFloat) {
+//        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+//        self.leftView = paddingView
+//        self.leftViewMode = .always
+//    }
+//}
+
 
 
 import UIKit
@@ -125,6 +280,7 @@ class LoginViewController: UIViewController {
             case .success:
                 DispatchQueue.main.async {
                     let homeViewController = HomeViewController()
+                    homeViewController.child = child
                     self?.navigationController?.pushViewController(homeViewController, animated: true)
                 }
             case .failure:
@@ -137,11 +293,10 @@ class LoginViewController: UIViewController {
     
     func presentAlertWithTitle(title: String, message: String, completion: (() -> Void)? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+            completion?()
+        })
         self.present(alert, animated: true, completion: nil)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            alert.dismiss(animated: true, completion: completion)
-        }
     }
 }
 
