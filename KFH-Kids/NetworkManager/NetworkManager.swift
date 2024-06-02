f58444dc3bd8aa3d68a59b38d248175f25814961
razanalshatti <83327@kfh.com>
2024-06-02 10:11:45 +0300

    network manager added

--- KFH-Kids/NetworkManager/NetworkManager.swift
+++ KFH-Kids/NetworkManager/NetworkManager.swift
@@ -26,8 +26,86 @@ class NetworkManager {
     }
     
     // MARK: GetTasks
+    func GetTasks(child: Child, completion: @escaping (Result<[Task], Error>) -> Void) {
+        let URL = baseURL + "api/Child/{childId}/GetTasks"
+        AF.request(URL, method: .get, parameters: child, encoder: URLEncodedFormParameterEncoder.default).responseDecodable(of: [Task].self) { response in
+            switch response.result {
+            case .success(let tasks):
+                completion(.success(tasks))
+            case .failure(let afError):
+                completion(.failure(afError as Error))
+            }
+        }
+    }
+    
+    // MARK: GetRewards
+    func GetRewards(child: Child, completion: @escaping (Result<[Task], Error>) -> Void) {
+        let URL = baseURL + "api/Child/{Id}/GetRewards"
+        AF.request(URL, method: .get, parameters: child, encoder: URLEncodedFormParameterEncoder.default).responseDecodable(of: [Task].self) { response in
+            switch response.result {
+            case .success(let rewards):
+                completion(.success(rewards))
+            case .failure(let afError):
+                completion(.failure(afError as Error))
+            }
+        }
+    }
+    
+    // MARK: task completion
+    func taskCompletion(child: Child, completion: @escaping (Result<[Task], Error>) -> Void) {
+        let URL = baseURL + "api/Child/{childId}/tasks/{taskId}/complete"
+        AF.request(URL, method: .put, parameters: child, encoder: URLEncodedFormParameterEncoder.default).responseDecodable(of: [Task].self) { response in
+            switch response.result {
+            case .success(let complete):
+                completion(.success(complete))
+            case .failure(let afError):
+                completion(.failure(afError as Error))
+            }
+        }
+    }
+    
+    // MARK: Balance
     func getTasks(child: Child, completion: @escaping (Result<[Task], Error>) -> Void) {
-        let URL = baseURL + "GetTasks"
+        let URL = baseURL + "api/Child/{childId}/balance"
+        AF.request(URL, method: .get, parameters: child, encoder: URLEncodedFormParameterEncoder.default).responseDecodable(of: [Task].self) { response in
+            switch response.result {
+            case .success(let balance):
+                completion(.success(balance))
+            case .failure(let afError):
+                completion(.failure(afError as Error))
+            }
+        }
+    }
+    
+    // MARK: GetPoints
+    func GetPoints(child: Child, completion: @escaping (Result<[Task], Error>) -> Void) {
+        let URL = baseURL + "api/Child/GetPoints/{ChildId}"
+        AF.request(URL, method: .get, parameters: child, encoder: URLEncodedFormParameterEncoder.default).responseDecodable(of: [Task].self) { response in
+            switch response.result {
+            case .success(let points):
+                completion(.success(points))
+            case .failure(let afError):
+                completion(.failure(afError as Error))
+            }
+        }
+    }
+    
+    // MARK: transfer
+    func transfer(child: Child, completion: @escaping (Result<[Task], Error>) -> Void) {
+        let URL = baseURL + "api/Child/{parentId}/transfer/{childId}"
+        AF.request(URL, method: .post, parameters: child, encoder: URLEncodedFormParameterEncoder.default).responseDecodable(of: [Task].self) { response in
+            switch response.result {
+            case .success(let transfer):
+                completion(.success(transfer))
+            case .failure(let afError):
+                completion(.failure(afError as Error))
+            }
+        }
+    }
+    
+    // MARK: ClaimRewards
+    func ClaimRewards(child: Child, completion: @escaping (Result<[Task], Error>) -> Void) {
+        let URL = baseURL + "api/Child/{childId}/claimedrewards"
         AF.request(URL, method: .get, parameters: child, encoder: URLEncodedFormParameterEncoder.default).responseDecodable(of: [Task].self) { response in
             switch response.result {
             case .success(let tasks):
